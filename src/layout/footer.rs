2bda8ceb39daf7b31409961fd76523ec9ddba08b
jane <jjt1525@outlook.com>
2023-10-02 16:52:12 +0800

    complete

--- src/layout/footer.rs
+++ src/layout/footer.rs
@@ -1,5 +1,5 @@
 use ratatui::style::{Color, Stylize};
-use ratatui::text::{Line, Span};
+use ratatui::text::Line;
 use ratatui::widgets::{Block, Borders, Padding, Paragraph, Widget};
 
 use crate::app::{Mode, State};
@@ -17,8 +17,8 @@ impl<'a> Footer<'a> {
 impl<'a> Widget for Footer<'a> {
     fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer) {
         let mode = match self.state.mode() {
-            Mode::Normal => " Normal ",
-            Mode::Search => " Search ",
+            Mode::Normal => " Normal ".bg(Color::Green),
+            Mode::Search => " Search ".bg(Color::Yellow),
         };
         let key = self.state.key();
         let key = if key.is_empty() {
@@ -26,23 +26,7 @@ impl<'a> Widget for Footer<'a> {
         } else {
             format!(" {} ", key)
         };
-        let width = (area.width as usize)
-            .saturating_sub(
-                mode.chars()
-                    .map(|char| if char.is_ascii() { 1 } else { 2 })
-                    .sum(),
-            )
-            .saturating_sub(
-                key.chars()
-                    .map(|char| if char.is_ascii() { 1 } else { 2 })
-                    .sum(),
-            )
-            .saturating_sub(2);
-        let text = vec![
-            mode.fg(Color::Black).bg(Color::White),
-            Span::from(" ".repeat(width)),
-            key.fg(Color::Black).bg(Color::White),
-        ];
+        let text = vec![mode.fg(Color::Black), key.fg(Color::Black).bg(Color::White)];
         let block = Block::new()
             .padding(Padding::horizontal(1))
             .borders(Borders::NONE);
